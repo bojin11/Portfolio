44393ad132283ab829a8f283ac369c8d7d58ea22
bojin11 <15011278723@163.com>
2024-09-02 19:30:29 +0100

    changes

--- src/components/ArtistBio/index.js
+++ src/components/ArtistBio/index.js
@@ -7,7 +7,7 @@ function ArtistBio() {
             <h1 id="heading">Bojin Zhou</h1>
             <h1 id="tagline">Bojin Zhou's Portfolio</h1>
                 <p role="bio" id="description">
-                "Bojin is an enthusiastic and dedicated graduate from King’s College London with a Computer Science degree. She has been working in the IT industry in total for five years. In her most recent role, she worked as a .Net developer delivering Application development and support using C# object-oriented language and Microsoft technologies. She was also responsible for improving the functionality of systems in close consultation with the business and end-users."
+                "Bojin is a Software Engineer with over 10 years of experience in delivering high-quality software solutions. She specializes in C#, .NET, Blazor, JavaScript, TypeScript, React, and Node.js, with a strong focus on application development and support. Her track record includes successfully leading multiple projects that have significantly improved system functionality and business operations."
                 </p>
             </div>
         </>
