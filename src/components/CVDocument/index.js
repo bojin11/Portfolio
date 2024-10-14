b66918988b41fcbd24a0d2699a252bacffe36c0f
bojin11 <15011278723@163.com>
2024-10-14 18:31:35 +0100

    Change the link

--- src/components/CVDocument/index.js
+++ src/components/CVDocument/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import doc from "../../documents/CV_Bojin Zhou_Software Developer_Sep-2024.pdf";
+import doc from "../../documents/Bojin Zhou_Fullstack_Developer_Sep_2024 (1).pdf";
 function CVDocument(){
 
  return  <p> To see more click <a style={{textDecoration:"underline"}} href={doc}>here</a> </p>
